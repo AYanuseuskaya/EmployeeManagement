aee01b525ce8fe08a6e273fc3b6a3b62ca8b096a
AYanuseuskaya <nastochka90@mail.ru>
2021-12-07 22:33:49 +0300

    Add UnitTest

--- TestProjectEmployeeManagement/UnitTest1.cs
+++ TestProjectEmployeeManagement/UnitTest1.cs
@@ -6,7 +6,7 @@ namespace TestProjectEmployeeManagement
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodEmployee()
         {
         }
     }

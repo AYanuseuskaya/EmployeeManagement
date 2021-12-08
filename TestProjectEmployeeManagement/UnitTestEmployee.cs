f73adc880aa16b81b5340eaeedff770c648d07ba
AYanuseuskaya <nastochka90@mail.ru>
2021-12-08 12:31:20 +0300

    Add unit tests

--- TestProjectEmployeeManagement/UnitTestEmployee.cs
+++ TestProjectEmployeeManagement/UnitTestEmployee.cs
@@ -6,31 +6,6 @@ namespace TestProjectEmployeeManagement
     [TestClass]
     public class UnitTestEmployee
     {
-        readonly Employee employee = new("Радцевич Константин Александрович", new(2020, 12, 15));
-        private const decimal resultEmployeeSalaryCalculation0Years = 50000;
-        [TestMethod]
-        public void TestMethodEmployeeSalaryCalculation_50000BaserateAnd0YearsOfWork_resultEmployeeSalaryCalculation0Years()
-        {
-            decimal result = employee.SalaryCalculation(System.DateTime.Now);
-            Assert.AreEqual(resultEmployeeSalaryCalculation0Years, result);
-        }
-
-        private const decimal resultEmployeeSalaryCalculation5Years = 57500;
-        [TestMethod]
-        public void TestMethodEmployeeSalaryCalculation_50000BaserateAnd5YearsOfWork_resultEmployeeSalaryCalculation5Years()
-        {
-            employee.AddOnDate = new(2016, 11, 1);
-            decimal result = employee.SalaryCalculation(System.DateTime.Now);
-            Assert.AreEqual(resultEmployeeSalaryCalculation5Years, result);
-        }
-
-        private const decimal resultEmployeeSalaryCalculation11Years = 65000;
-        [TestMethod]
-        public void TestMethodEmployeeSalaryCalculation_50000BaserateAnd11YearsOfWork_resultEmployeeSalaryCalculation11Years()
-        {
-            employee.AddOnDate = new(2009, 12, 15);
-            decimal result = employee.SalaryCalculation(System.DateTime.Now);
-            Assert.AreEqual(resultEmployeeSalaryCalculation11Years, result);
-        }
+        
     }
 }
